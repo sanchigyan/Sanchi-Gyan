0f83fd7346e73ebb693e9ec6d78fb7ce1fc916da
sanchigyan <sanchigyan@gmail.com>
2025-10-08 16:48:26 +0400

    feat: Added hero section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Hero from "@/components/home/hero";
+import Navbar from "@/components/layout/Navber";
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <main>
-        
+        <Hero/>
       </main>
     </div>
   );
